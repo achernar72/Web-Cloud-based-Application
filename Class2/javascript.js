0177866ba33eec48b63c1a94bf52a4f9631f2b4f
achernar72 <sankarasettyavinash@gmail.com>
2017-07-06 18:32:37 -0500

    the files are added

---
+++ Class2/javascript.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var limit=3;
+
+    document.getElementById("demo").innerHTML = Math.random();
+    if(Math.random=1)
+    {
+        print("rock")
+    }
+    else
+        print(paper)
+}
